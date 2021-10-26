2933c177fcadba3990cd45c8da545aaa84c290e2
冯正本18682012424 <121603757@qq.com>
2021-10-26 07:59:29 +0000

    删除文件 laqu.xls

--- study/PHP echo print/PHP print 语句.php
+++
@@ -1,8 +0,0 @@
-<?php
-print "<h2>PHP 很有趣!</h2>";
-print "Hello world!<br>";
-print ("可以用括号<br>");//可以用括号，内面必须有引号
-print '可以用单引号<br>';//可以用单引号
-print "我要学习 PHP!";//只允许输出一个字符串，
-print "我要学习 PHP!"."结尾";//print也可以用.
-?>
